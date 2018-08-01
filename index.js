f29ddb532c2397b2e7f92b994f1e54a495e29309
gunturkh <guntur.kh@gmail.com>
2018-08-01 15:12:20 +0700

    Displaying only followers name from GunturKH Account

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fetch = require ('node-fetch');
-fetch('https://api.github.com/users/gunturkh')
+const chalk = require ('chalk');
+fetch('https://api.github.com/users/gunturkh/followers')
     .then(res => res.json())
-    .then(json =>  console.log(json));
+    .then(json => {
+	    json.forEach(item => { console.log(item.login)
+            })
+    })
+
 
