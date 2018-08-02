97c7a7a44659dfb1d7755f8ff92d2ec6b7a2f53b
gunturkh <guntur.kh@gmail.com>
2018-08-02 14:26:12 +0700

    Refactoring code

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 const fetch = require('node-fetch');
-const gitURL = 'https://api.github.com/users/gunturkh/followers'
-const swapiURL = 'https://swapi.co/api/people/'
 const readline = require('readline');
-const log = (data) => { console.log(data) };
-const defaultMessage =
-  (
-    `Hello!>> 
-   Menu :
-   github-follower
-   star-war
-   exit
-   help
-  `)
-const helpMessage =
-  (
-  `Available option are:
-  github-follower
-  star-war
-  exit
-  help
-  `)
+
+const githubURL = 'https://api.github.com/users/gunturkh/followers'
+const swapiURL = 'https://swapi.co/api/people/'
+
+const log = data => console.log(data);
+
+const logSwapi = ({ name, birth_year }) => {
+  log(`Character name: ${name}, Birth year: ${birth_year}`)
+}
+
+const logGithub = ({ login, id }) => {
+  log(`Name: ${login}, id: ${id}`)
+}
+
+const fetchGitHub = () => {
+  fetch(githubURL)
+    .then(res => res.json())
+    .then(json => json.forEach(logGithub))
+}
+
+const fetchSWAPI = () => {
+  fetch(swapiURL)
+    .then(res => res.json())
+    .then(json => json.results.forEach(logSwapi))
+}
+
+const helpMessage = `
+Menu:
+github-followers
+star-wars
+exit
+help
+`
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: defaultMessage
+  prompt: helpMessage
 });
 
 rl.prompt();
 
-rl.on('line', (line) => {
+rl.on('line', line => {
   const inputLine = line.trim();
-  if (inputLine === 'help') { log(helpMessage) }
-  else if (inputLine === 'github-follower') {
-    fetch(gitURL)
-      .then(res => res.json())
-      .then(json => json.forEach(item => { log(`Name: ${item.login}, id: ${item.id}`) }))
+  if (inputLine === 'help') log(helpMessage)
+  else if (inputLine === 'github-followers') fetchGitHub()
+  else if (inputLine === 'star-wars') fetchSWAPI()
+  else if (inputLine === 'exit') log('Have a great day!')
+  else {
+    log(`Say what? I might have heard '${inputLine}. Type help for additional information`)
+    process.exit(0)
   }
-  else if (inputLine === 'star-war') {
-    fetch(swapiURL)
-      .then(res => res.json())
-      .then(json => json.results.forEach(item => { log(`Character name: ${item.name}, Birth year: ${item.birth_year}`) }))
-  }
-  else if (inputLine === 'exit') {log('Have a great day!'); process.exit(0)}
-  else { log(`Say what? I might have heard '${line.trim()}. Type help for additional information'`);}
 })
-
-
-
-
